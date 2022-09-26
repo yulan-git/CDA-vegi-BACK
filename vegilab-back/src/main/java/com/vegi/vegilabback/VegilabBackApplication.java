0f4b51a36c26cf43949740334afc5c3fdfb34ccc
Lafferrière <flora.lafferriere@gmail.com>
2022-09-26 22:17:25 +0200

    first commit

---
+++ vegilab-back/src/main/java/com/vegi/vegilabback/VegilabBackApplication.java
@@ -0,0 +1,13 @@
+package com.vegi.vegilabback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegilabBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegilabBackApplication.class, args);
+	}
+
+}
